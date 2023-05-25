e34f15932bad1b62690e336ca966092b04a4ae63
Bushra Taibani <bushrataibani1@gmail.com>
2023-05-25 17:58:02 +0530

    static data list for post

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 import Home from './src/screens/Home';
 import Post from './src/components/Post';
+import feed from './assets/data/feed';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        {/* <Home /> */}
-        <Post />
+        <ScrollView>
+          <Home />
+          {feed?.map(item => (
+            <Post key={item?.key} props={item} />
+          ))}
+        </ScrollView>
       </SafeAreaView>
     </>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,36 +2,44 @@ import React from 'react';
 import {Image, Text, View} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = ({props}) => {
+  const {
+    image,
+    type,
+    title,
+    bed,
+    bedroom,
+    oldPrice,
+    newPrice,
+    totalPrice,
+    coordinate,
+  } = props;
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         style={styles.image}
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: image,
         }}
       />
 
       {/* Bed & bedroom */}
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>{`${bed} bed ${bedroom} bedroom`}</Text>
 
       {/* Type & Description */}
       <Text style={styles.description} numberOfLines={3}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
+        {type}. {title}
       </Text>
 
       {/* Old Price & New Price */}
       <Text style={styles.price}>
-        <Text style={styles.oldPrice}>$36 </Text>
-        <Text style={styles.newPrice}> $30</Text> / night
+        <Text style={styles.oldPrice}>{`${oldPrice}`} </Text>
+        <Text style={styles.newPrice}> {`${newPrice}`}</Text> / night
       </Text>
 
       {/* Total Price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>{`${totalPrice} total`}</Text>
     </View>
   );
 };
